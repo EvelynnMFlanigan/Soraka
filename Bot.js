e006b6be7c05e6a7fbf9ae1ce9107bcea2d4f1cf
Ryan <rflanig1@uncc.edu>
2020-06-09 21:22:42 -0700

    revised and added packages

--- Bot.js
+++ Bot.js
@@ -1,10 +1,27 @@
-// JavaScript source code
+//discord requirements
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+//cheerio and axios for web scraping
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+//requirements to parse json files
+var fs = require("fs");
+var content = fs.readFileSync("tokens.json")
+var tokens = JSON.parse(content)
+var messageData;
+const url = 'https://na.leagueoflegends.com/en-us/news/game-updates/';
+var schedule = require('node-schedule')
+
+
+
+
+
+
 //league kayn api stuff
 const { Kayn, REGIONS } = require('kayn');
-const kayn = Kayn('RGAPI-e8d5db05-b6e6-4577-a0f1-26b44667fa1a')({
+const kayn = Kayn(tokens.leagueToken)({
     region: REGIONS.NORTH_AMERICA,
     apiURLPrefix: 'https://%s.api.riotgames.com',
     locale: 'en_US',
@@ -30,92 +47,47 @@ const kayn = Kayn('RGAPI-e8d5db05-b6e6-4577-a0f1-26b44667fa1a')({
     },
 })
 
+//confirms the bot is connected to discord prints message to a console with bot name
 client.on('ready', () => {
     console.log("Connected as " + client.user.tag);
+var j = schedule.scheduleJob({hour:21, minute: 10}, function(){
 
+    axios.get(url).then(response => {
+        let getData = html => {
+            const $ = cheerio.load(html);
+            messageData = "https://na.leagueoflegends.com" + $('a.style__Wrapper-i44rc3-0.style__ResponsiveWrapper-i44rc3-13.gkCnQM.isVisible').attr('href')
+        }
+
+        getData(response.data)
+    });
+})
 })
 
 client.on('message', (receivedMessage) => {
-//if(receivedMessage.author = client.user){
-  //  return
-//}
+       //splits message into command and person
     if(receivedMessage.content.startsWith("!")) {
-        let fullCommand = receivedMessage.content.substr(1) // Remove the leading exclamation mark
-        let splitCommand = fullCommand.split(" ") // Split the message up in to pieces for each space
+        var fullCommand = receivedMessage.content.substr(1) // Remove the leading exclamation mark
+        var splitCommand = fullCommand.split(" ") // Split the message up in to pieces for each space
         var support = splitCommand[0] // The first word directly after the exclamation is the command
         var theSummoner = splitCommand[1] //the name of the summoner to be used
     }
 
 if(support == "details" || support == "Details" ) {
 
-    let tempSummoner =  theSummoner
-
-    if (tempSummoner == "Fenris" || tempSummoner == "fenris") {
-        kayn.Summoner.by.name("3MareepInACoat").callback(function (err, summoner) {
-            receivedMessage.channel.send("Summoner name: " + summoner.name + "     Level: " + summoner.summonerLevel)
-        });
-    }
-    else if (tempSummoner == "BlueTriforce" || tempSummoner == "bluetriforce"){
-        kayn.Summoner.by.name("BlueTriforce").callback(function (err, summoner) {
-            receivedMessage.channel.send("Summoner name: " + summoner.name + "     Level: " + summoner.summonerLevel)
-        });
-    }
-    else if (tempSummoner == "W1Z4RD" || tempSummoner == "w1z4rd" || tempSummoner == "wizard"){
-        kayn.Summoner.by.name("W1Z4RD").callback(function (err, summoner) {
-            receivedMessage.channel.send("Summoner name: " + summoner.name + "     Level: " + summoner.summonerLevel)
-        });
-    }
-    else if (tempSummoner == "Kogomoru" || tempSummoner == "kogomoru"){
-        kayn.Summoner.by.name("Kogomoru").callback(function (err, summoner) {
-            receivedMessage.channel.send("Summoner name: " + summoner.name + "     Level: " + summoner.summonerLevel)
-        });
-    }
-    else if (tempSummoner == "yogurt" || tempSummoner == "Yogurt"){
-        kayn.Summoner.by.name("Baby Toes").callback(function (err, summoner) {
-            receivedMessage.channel.send("Summoner name: " + summoner.name + "     Level: " + summoner.summonerLevel)
-        });
-    }
-    else if (tempSummoner == "ChungaWunga" || tempSummoner == "chungawunga"){
-        kayn.Summoner.by.name("Piece Biscuit").callback(function (err, summoner) {
-            receivedMessage.channel.send("Summoner name: " + summoner.name + "     Level: " + summoner.summonerLevel)
-        });
-    }
-    else if (tempSummoner == "Gungfusi" || tempSummoner == "gungfusi"){
-        kayn.Summoner.by.name("Gungfusi").callback(function (err, summoner) {
-            receivedMessage.channel.send("Summoner name: " + summoner.name + "     Level: " + summoner.summonerLevel)
-        });
-    }
-    else if (tempSummoner == "Botconman" || tempSummoner == "botconman"){
-        kayn.Summoner.by.name("Botconman").callback(function (err, summoner) {
-            receivedMessage.channel.send("Summoner name: " + summoner.name + "     Level: " + summoner.summonerLevel)
-        });
-    }
-     //Only works on one word names no spaces
-    else{
-        kayn.Summoner.by.name(tempSummoner).callback(function (err, summoner) {
-            receivedMessage.channel.send("Summoner name: " + summoner.name + "     Level: " + summoner.summonerLevel)
-        });
-    }
+    kayn.Summoner.by.name(theSummoner).callback(function (err, summoner) {
+        receivedMessage.channel.send("Summoner name: " + summoner.name + "     Level: " + summoner.summonerLevel)
+    });
 }
-    if(support =="gamedetails" || support == "game"){
-        let tempSummoner = theSummoner
-       let kayn.Summoner.by.name(tempSummoner).callback(function (err, summoner) {
+if(support =="gamedetails" || support == "game"){
+    let gameInfo;
+       kayn.Summoner.by.name(theSummoner).callback(function (err, summoner) {
+           kayn.CurrentGame.by.summonerID(summoner.id).callback(function (err, match) {
 
+           })
         });
-
-        kayn.CurrentGame.by.summonerID(tempId).callback(function (err, match) {
-            recievedMessage.channel.send(CurrentGame.participants )
-
-        })
-
     }
 });
 
 
 
-// Get your bot's secret token from:
-// https://discordapp.com/developers/applications/
-// Click on your application -> Bot -> Token -> "Click to Reveal Token"
-bot_secret_token = "NDY1NzA4NjAyMzU1ODc1ODQw.XY0hTA.hkKN50n9DUMBrEFnVsYugkKAlgk"
-
-client.login(bot_secret_token)
+client.login(tokens.discordToken)
